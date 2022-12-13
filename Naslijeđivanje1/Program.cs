28a0224e14e8052d15b8d5b3011d63224eccb9d5
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-12-13 18:41:04 +0100

    Add files via upload

---
+++ Naslijeđivanje1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naslijeđivanje1
+{
+    class NebeskoTijelo
+    {
+        double brzinaOkoOsi, brzinaOkoSunca;
+
+        public double BrzinaOkoOsi { get => brzinaOkoOsi; set => brzinaOkoOsi = value; }
+        public double BrzinaOkoSunca { get => brzinaOkoSunca; set => brzinaOkoSunca = value; }
+    }
+    class Planet : NebeskoTijelo
+    {
+        public Planet(double brzinaOkoOsi, double brzinaOkoSunca)
+        {
+            BrzinaOkoOsi = brzinaOkoOsi;
+            BrzinaOkoSunca = brzinaOkoSunca;
+        }
+
+        public override string ToString()
+        {
+            return "Brzina planeta oko osi je: " + BrzinaOkoOsi + " kmh, a brzina oko sunca mu je: " + BrzinaOkoSunca + " kmh.";
+        }
+    }
+    class Satelit : NebeskoTijelo
+    {
+        public Satelit(double brzinaOkoOsi, double brzinaOkoSunca)
+        {
+            BrzinaOkoOsi = brzinaOkoOsi;
+            BrzinaOkoSunca = brzinaOkoSunca;
+        }
+
+        public override string ToString()
+        {
+            return "Brzina satelita oko osi je: " + BrzinaOkoOsi + " kmh, a brzina oko sunca mu je: " + BrzinaOkoSunca + " kmh.";
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Planet Zemlja = new Planet(1000, 10000);
+                Satelit Mjesec = new Satelit(100, 1000);
+                Console.WriteLine(Zemlja.ToString());
+                Console.WriteLine(Mjesec.ToString());
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
